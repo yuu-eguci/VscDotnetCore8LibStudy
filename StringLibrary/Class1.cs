6efd8820c08f3be54a71b495daae2d37b5dd1471
yuu-eguci <yuu.eguci@gmail.com>
2024-01-12 10:15:57 +0900

    ✨ Class1.cs に クラス ライブラリ UtilityLibraries.StringLibrary を実装

--- StringLibrary/Class1.cs
+++ StringLibrary/Class1.cs
@@ -1,6 +1,15 @@
-﻿namespace StringLibrary;
+﻿namespace UtilityLibraries;
 
-public class Class1
+public static class StringLibrary
 {
+    public static bool StartsWithUpper(this string? str)
+    {
+        // 現在の文字列のインスタンスが大文字で始まるかどうかを示す Boolean 値を返します。
 
+        if (string.IsNullOrWhiteSpace(str))
+            return false;
+
+        char ch = str[0];
+        return char.IsUpper(ch);
+    }
 }
